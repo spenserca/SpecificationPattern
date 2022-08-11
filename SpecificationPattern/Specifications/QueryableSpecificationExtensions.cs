caa364e50a006f5363d87f6cda9bf3b1b6060528
Spenser Andreassen <spenserca@gmail.com>
2022-08-10 21:06:31 -0500

    add class constraint on method

--- SpecificationPattern/Specifications/QueryableSpecificationExtensions.cs
+++ SpecificationPattern/Specifications/QueryableSpecificationExtensions.cs
@@ -1,13 +1,14 @@
 ﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SpecificationPattern.Specifications
 {
     public static class QueryableSpecificationExtensions
     {
         public static IQueryable<TEntity> Where<TEntity>(this IQueryable<TEntity> query,
-            ISpecification<TEntity> specification)
+            ISpecification<TEntity> specification) where TEntity : class
         {
-            var queryableResultWithIncludes = specification.Includes
+            specification.Includes
                 .Aggregate(query,
                     (current, include) => current.Include(include));
 
